21b91bcdadc87af604f64cf794c6726afd4f1922
macluiggy <ferrinluiggy@gmail.com>
2021-09-16 13:53:39 -0500

    djjd

--- ARCCHIVO DE LUIGGY.js
+++ ARCCHIVO DE LUIGGY.js
@@ -1,2 +1,3 @@
 //ESTE ARCHIVO ES DE LUIGGY
 //holadhhddd
+////MAJOR.MINOR.PATCH SemVer, esto sirve para guiarse con las versiones de un proyecto, MAJOR indica que se hizo un cambio que en la que las nuevas funcionalidades solo seran comparible desde MAJOR, MINOR quiere decir que se añadieron funcionalidades que si son compatibles con verisiones aneriores, mientras que PATCH nos trata de decir que se arreglaron errores que son compatibles con versiones anteriores. En otras palabras los PATCHES son correciones de erroes de, los MINOR son nuevas funciones que no rompen compatibilidad con versiones anteriores y MAJOR añade cambios que si rompen con las versiones anteriores, o no son compatibles con ella.
